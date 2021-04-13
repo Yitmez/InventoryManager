c8aa0a6013747b7924dc05bcbecf123d376f5968
Yitmez, K <yitmez@live.de>
2021-04-13 23:15:23 +0200

    Anmelde-Button im Header hinzugefügt

---
+++ Wooja Inventory Manager/Models/Calculation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wooja_Inventory_Manager.Models
+{
+    public class Calculation
+    {
+
+        public long Id { get; set; }
+        public int Count { get; set; }
+        public long Result { get; set; }
+    }
+}
