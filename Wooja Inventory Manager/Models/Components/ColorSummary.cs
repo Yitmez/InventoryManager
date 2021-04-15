9753502d33bb580619363b950bc6d42903e22fbe
Yitmez, K <yitmez@live.de>
2021-04-15 10:03:13 +0200

    ViewComponent

---
+++ Wooja Inventory Manager/Models/Components/ColorSummary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wooja_Inventory_Manager.Models.Components
+{
+    public class ColorSummary : ViewComponent
+    {
+        private Color data;
+
+        public ColorSummary(Color cdata)
+        {
+            data = cdata;
+        }
+
+        // IViewComponentResult für... ginge auch.  ?? prüfen
+        public string Invoke() //parameter (string themeName) könnte auch gehen 
+                              
+        {
+            ViewBag.Theme = "Extra Info, könnte per Parameter kommen";
+            return ("Weitergabe von Informationen");
+        }
+    }
+}
---
+++ Wooja Inventory Manager/Models/Components/ColorsData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wooja_Inventory_Manager.Models.Components
+{
+    public class ColorsData
+    {
+        private List<Color> colors = new List<Color> {    // Alle Colors in Color umstellen
+ new Color { Name = "Blau", HexCode = "23423"},
+ new Color { Name = "Gelb", HexCode = "34343"},
+ new Color { Name = "Rot", HexCode = "34342" },
+ new Color { Name = "Grün", HexCode = "0X3434"}
+ };
+        public IEnumerable<Color> Colors => colors;
+        public void AddColor(Color newColor)
+        {
+            colors.Add(newColor);
+        }
+    }
+}
